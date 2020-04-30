e0af9727d617d5042a17cc8655c41e8544a69e09
ywangdr <ywangdr@stanford.edu>
2020-04-29 20:21:11 -0700

    fix minor bug, refactor code

--- src/train.py
+++ src/train.py
@@ -5,13 +5,22 @@ import logging
 import numpy as np
 
 
-def train(model, features, labels, train_mask, test_mask, args):
-    device = torch.device('cuda:{}'.format(args.gpu) if torch.cuda.is_available() else 'cpu')
+def send_to_device(features, labels, train_mask, test_mask, device):
     train_features = torch.from_numpy(features[train_mask]).float().to(device)
     test_features = torch.from_numpy(features[test_mask]).float().to(device)
     train_labels = torch.from_numpy(labels[train_mask]).long().to(device)
     test_labels = torch.from_numpy(features[test_mask]).to(device)
-    epoch = args.epoch
+    labels = torch.from_numpy(labels).long().to(device)
+    features = torch.from_numpy(features).float().to(device)
+    train_mask = torch.from_numpy(train_mask).bool().to(device)
+    test_mask = torch.from_numpy(test_mask).bool().to(device)
+    return features, train_features, test_features, train_labels, test_labels, labels, train_mask, test_mask
+
+
+def train(model, features, labels, train_mask, test_mask, args):
+    device = torch.device('cuda:{}'.format(args.gpu) if torch.cuda.is_available() else 'cpu')
+    data = send_to_device(features, labels, train_mask, test_mask, device)
+    features, train_features, test_features, train_labels, test_labels, labels, train_mask, test_mask = data
     criterion = torch.nn.functional.cross_entropy
     optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, weight_decay=args.l2)
     n_train_samples = train_labels.shape[0]
@@ -19,7 +28,7 @@ def train(model, features, labels, train_mask, test_mask, args):
     bs = args.bs
     best_test_acc = 0
     best_test_acc_epoch = -1
-    for step in range(epoch):
+    for step in range(args.epoch):
         model.train()
         shuffled_index = np.random.permutation(n_train_samples)
         count = 0
@@ -55,5 +64,5 @@ def train(model, features, labels, train_mask, test_mask, args):
 
 def compute_acc(predictions, labels):
     correct_prediction = (torch.argmax(predictions, dim=1) == labels)
-    acc = correct_prediction.float().sum().item() / len(labels)
+    acc = correct_prediction.float().sum().item() / labels.size(0)
     return acc
